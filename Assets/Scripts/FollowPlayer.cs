f991f095bedc1d44ded3eed159650e66f0f354df
Jose Morales <jmoralesgutierr4264@conestogac.on.ca>
2025-07-15 09:19:49 -0400

    Smooth the Camera with LateUpdate

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,7 +12,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // Offset the camera behind the player adding to the player's position
         transform.position = player.transform.position + offset; 
